7dd40a3d7f769080f485b07d5eb30e931f7ef2d9
Herbert V <herbert.videla@tektonlabs.com>
2016-08-17 15:46:12 -0500

    styles in the page

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import {Link} from 'react-router';
 
+const logo = require('../images/tesis.jpg')
+
 export default class Home extends React.Component {
   constructor() {
     super();
@@ -9,8 +11,11 @@ export default class Home extends React.Component {
   render() {
     return (
       <div class='home'>
-      	<h1>React Facilito, Pasa y Firma</h1>
-      	<Link to='sign'>Firma ahora !</Link>
+        <img class='center-block' src={logo} alt="Logo"/>
+      	<h1 class='text-center'>React Facilito, Pasa y Firma</h1>
+      	<button class="btn btn-default btn-lg center-block">
+          <Link to='sign'>Firma ahora !</Link>
+        </button>
       </div>
     );
   }
